fd2978579797eba598ea75f9280e34d15b84201f
robertocarroll <rob@wearethoughtfox.com>
2024-06-10 11:59:46 +0100

    Remove if statement

--- src/render.php
+++ src/render.php
@@ -3,21 +3,18 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-  <?php
-// First, determine the current post type
-$current_post_type = get_post_type($post->ID);
+<?php
+// Always setting the target post type for the query to 'speaker'
+$target_post_type = 'speaker';
 
-// Depending on the current post type, set the target post type for the query
-$target_post_type = ($current_post_type === 'session') ? 'speaker' : 'session';
-
-// Get the terms related to the current post
+// Get the terms related to the current session post 
 $terms = get_the_terms($post->ID, 'conference');
 if ($terms && !is_wp_error($terms)) {
     $term_ids = wp_list_pluck($terms, 'term_id');
 
-    // Setup the query arguments
+    // Setup the query arguments to get speakers
     $args = array(
-        'post_type' => $target_post_type,  // Dynamically set to either 'session' or 'speaker'
+        'post_type' => $target_post_type,
         'tax_query' => array(
             array(
                 'taxonomy' => 'conference',
@@ -25,21 +22,21 @@ if ($terms && !is_wp_error($terms)) {
                 'terms'    => $term_ids,
             ),
         ),
-        'posts_per_page' => -1  // Retrieve all matching posts
+        'posts_per_page' => -1  // Retrieve all matching speakers
     );
 
-    // Execute the query
-    $related_posts = new WP_Query($args);
-    if ($related_posts->have_posts()) {
-        // Dynamically display heading based on post type
-        echo '<h3>' . ucfirst($target_post_type) . 's</h3><ul>';
-        while ($related_posts->have_posts()) : $related_posts->the_post();
+    // Execute the query for speakers
+    $related_speakers = new WP_Query($args);
+    if ($related_speakers->have_posts()) {
+        echo '<h3>Speakers</h3><ul>';
+        while ($related_speakers->have_posts()) : $related_speakers->the_post();
             echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
         endwhile;
         echo '</ul>';
     } else {
-        echo '<p>No related ' . $target_post_type . 's found.</p>';
+        echo '<p>No related speakers found.</p>';
     }
     wp_reset_postdata();  // Reset post data after custom query
 }
 ?>
+
