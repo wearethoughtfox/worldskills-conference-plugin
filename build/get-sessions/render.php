e5dbae599e54292cad7ec662dfdb16738a2e8353
robertocarroll <rob@wearethoughtfox.com>
2024-08-27 15:55:28 +0100

    New fix for php warnings on speakers page

--- build/get-sessions/render.php
+++ build/get-sessions/render.php
@@ -5,20 +5,22 @@
 ?>
 <?php
 
-// Ensure we have access to the global $post object
-global $post;
+// Get the current post, works for any post type
+$current_object = get_queried_object();
 
-// Check if $post is set and is an object before proceeding
-if (isset($post) && is_object($post)) {
+// Check if we have a valid post object
+if ($current_object instanceof WP_Post) {
 
 // Set the target post type for the query to 'session'
 $target_post_type = 'session';
 
+if (taxonomy_exists('conference')) {
 // Get the terms related to the current speaker post
-$terms = get_the_terms($post->ID, 'conference');
+$terms = get_the_terms($current_post->ID, 'conference');
+
 if ($terms && !is_wp_error($terms) && !empty($terms)) {
     $term_ids = wp_list_pluck($terms, 'term_id');
-
+    
     // Setup the query arguments to get sessions
     $args = array(
         'post_type' => $target_post_type,
@@ -37,12 +39,16 @@ if ($terms && !is_wp_error($terms) && !empty($terms)) {
     if ($related_sessions->have_posts()) {
         echo '<h3 class="wp-block-heading has-standard-font-size" style="font-weight:900; text-transform:uppercase ">Sessions</h3><ul class="ws-date-list no-margin-block-start">';
         while ($related_sessions->have_posts()) : $related_sessions->the_post();
-            echo '<li><a style="font-weight:bold;" href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+            echo '<li><a style="font-weight:bold;" href="' . esc_url(get_permalink()) . '">' . esc_html(get_the_title()) . '</a></li>';
         endwhile;
         echo '</ul>';
     } else {
         echo '<p class="screen-reader-text">This speaker does not seem to be involved in any sessions.</p>';
     }
+
+} else {
+    echo '<p class="screen-reader-text">Conference taxonomy does not exist.</p>';
+}
     wp_reset_postdata();  // Reset post data after custom query
 }
 
